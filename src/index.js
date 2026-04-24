430f3063c49b2071a2263efa7350dda0b56abda6
Jjoongrami910 <doktaviane910@gmail.com>
2026-04-24 10:52:33 +0700

    .

--- src/index.js
+++ src/index.js
@@ -29,6 +29,4 @@ app.use("/", userRoutes);
 app.use("/", ticketingRoutes);
 app.use("/", uploadRoutes);
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports = app;
